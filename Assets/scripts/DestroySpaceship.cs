43803860e5ce4c02e46b1e172e5c9dcda38b05f6
Josue Rivera <josuedr11@gmail.com>
2017-03-18 16:55:46 -0430

    Spaceship now explodes when it collides with an asteroid

---
+++ Assets/scripts/DestroySpaceship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySpaceship : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "asteroid")
+		{
+			Destroy(coll.gameObject);
+			Destroy(this.gameObject);
+			Debug.Log("Game over");
+		}
+
+	}
+
+	
+}
