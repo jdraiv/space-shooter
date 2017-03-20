b46e418febc6a05ae176b58d10fbfd67fdd95b3b
Josue Rivera <josuedr11@gmail.com>
2017-03-20 01:26:05 -0430

    Added asteroid animation

--- Assets/scripts/BulletDetection.cs
+++ Assets/scripts/BulletDetection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletDetection : MonoBehaviour {
+	public int health = 2;
 
 	// Update is called once per frame
 	void Update () {
@@ -11,10 +12,20 @@ public class BulletDetection : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
+
 		if (coll.gameObject.tag == "bullet")
 		{
-			Destroy(coll.gameObject);
-			Destroy(this.gameObject);
+			if (health == 1)
+			{
+				Destroy(coll.gameObject);
+			    Destroy(this.gameObject);
+			}
+			else
+			{
+				health -= 1;
+				Destroy(coll.gameObject);
+				
+			}
 		}
 
 		if (coll.gameObject.tag == "border")
